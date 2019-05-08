1b459b1888adb6eb98af234647c9c992db3fcda0
Jocel Sabellano <enjoyjocel@gmail.com>
2019-05-08 15:42:27 -0500

    Update Write-Log.ps1

--- Write-Log.ps1
+++ Write-Log.ps1
@@ -1,18 +1,28 @@
-.<#
-.SYNOPSIS
-    WL (Write-Log)
-.DESCRIPTION
-    Writes a log file to directory same as the script. Also outputs the entry to the console.
-.EXAMPLE
-    PS C:\> WL "Connecting to WSUS Server"
-    This will output "050719:142801 - Connecting to WSUS Server" in both to a file and in the console. 
-.INPUTS
-    Inputs (if any)
-.OUTPUTS
-    Output (if any)
-.NOTES
-    Usually goes as the first function of your script.
-    You can copy the one liner below and just ommit this help section. Feel free to modify to your liking. :=`)
-#>
+Function WL() {
 
-$SN = $MyInvocation.MyCommand.Name; Function WL($LE) { $LN = (Get-Date -Format "MMddyy:HHmmss") + " - $LE"; $LN | Out-File -FilePath "$PSScriptRoot\$SN-log.txt" -Append -NoClobber -Encoding "Default"; $LN }
+    <#
+    .SYNOPSIS
+        WL (Write-Log)
+    .DESCRIPTION
+        Writes a log file to directory same as the script. Also outputs the entry to the console.
+    .EXAMPLE
+        PS C:\> WL "Connecting to WSUS Server"
+        This will output "050719:142801 - Connecting to WSUS Server" in both to a file and in the console. 
+    .INPUTS
+        Inputs (if any)
+    .OUTPUTS
+        Output (if any)
+    .NOTES
+        Usually goes as the first function of your script.
+    #>
+
+    param(
+        [Parameter(Mandatory=$true)]
+        [String]
+        $LE
+    )
+    $SN = $MyInvocation.MyCommand.Name;
+    $LN = (Get-Date -Format "MMddyy:HHmmss") + " - $LE";
+    $LN | Out-File -FilePath "$PSScriptRoot\$SN-log.txt" -Append -NoClobber -Encoding "Default";
+    $LN
+}
