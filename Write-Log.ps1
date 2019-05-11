6d4d4a4939f8ad2ac1032ba9019da42e5f11c717
Jocel Sabellano <enjoyjocel@live.com>
2019-05-11 18:32:45 -0500

    Update Write-Log.ps1
    .

--- Write-Log.ps1
+++ Write-Log.ps1
@@ -1,4 +1,22 @@
+<<<<<<< HEAD
 Function WL() {
+=======
+.<#
+.SYNOPSIS
+    WL (Write-Log)
+.DESCRIPTION
+    Writes a log file to directory same as the script. Also outputs the entry to the console.
+.EXAMPLE
+    PS C:\> WL "Connecting to WSUS Server"
+    This will output "050719:142801 - Connecting to WSUS Server" in both to a file and in the console. 
+.INPUTS
+    Inputs (if any)
+.OUTPUTS
+    Output (if any)
+.NOTES
+    Usually goes as the first function of your script. 
+#>
+>>>>>>> parent of d5b67f7... Update Write-Log.ps1
 
     <#
     .SYNOPSIS
